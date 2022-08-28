e95b98769398d2887f198ad53c95448133527ac3
aryan-dubey <itsaryanborn2rock@gmail.com>
2022-08-29 05:06:32 +0530

    Initial Commit

---
+++ multiclientweb/src/main/java/com/multiclientweb/MulticlientwebApplication.java
@@ -0,0 +1,13 @@
+package com.multiclientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulticlientwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulticlientwebApplication.class, args);
+	}
+
+}
---
+++ multiclientweb/src/test/java/com/multiclientweb/MulticlientwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiclientweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulticlientwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
