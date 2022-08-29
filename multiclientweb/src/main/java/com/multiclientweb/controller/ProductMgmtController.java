68787da69e6f0eeacc853a47f078b5921a7a5208
aryan-dubey <itsaryanborn2rock@gmail.com>
2022-08-29 17:39:59 +0530

    ProductMgmt Controller Created

--- multiclientweb/src/main/java/com/multiclientweb/controller/ProductMgmtController.java
+++ multiclientweb/src/main/java/com/multiclientweb/controller/ProductMgmtController.java
@@ -1,5 +1,29 @@
 package com.multiclientweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.multiclientweb.entity.ProductMgmt;
+import com.multiclientweb.services.ProductMgmtServices;
+
+@RestController
 public class ProductMgmtController {
+	@Autowired
+	ProductMgmtServices ProductMgmtservices;
+	public ResponseEntity addProduct(@RequestBody ProductMgmt productmgmt,@PathVariable int vendorId) 
+	{		
+        if( ProductMgmtservices.add(productmgmt,vendorId))
+        {
+            return ResponseEntity.status(HttpStatus.OK).body(productmgmt);
+
+        }
+
+        	return ResponseEntity.status(HttpStatus.FOUND).body("Product already exist");
+		}
+	
 
 }
