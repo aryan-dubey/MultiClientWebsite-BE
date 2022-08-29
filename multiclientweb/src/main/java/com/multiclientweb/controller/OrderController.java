542cbb7c772935711d8bf5dbb6dac936aceb6654
lenovo <lenovo@DESKTOP-81D4MVI>
2022-08-29 16:20:04 +0530

    Order id created

--- multiclientweb/src/main/java/com/multiclientweb/controller/OrderController.java
+++ multiclientweb/src/main/java/com/multiclientweb/controller/OrderController.java
@@ -1,5 +1,14 @@
 package com.multiclientweb.controller;
 
 public class OrderController {
+	private int orderid;
+
+	public int getOrderid() {
+		return orderid;
+	}
+
+	public void setOrderid(int orderid) {
+		this.orderid = orderid;
+	}
 
 }
