758da92deb76284e078c735455c167dc8b023052
aryan-dubey <itsaryanborn2rock@gmail.com>
2022-08-29 17:40:21 +0530

    ProductMgmt Repository Created

--- multiclientweb/src/main/java/com/multiclientweb/repository/ProductMgmtRepository.java
+++ multiclientweb/src/main/java/com/multiclientweb/repository/ProductMgmtRepository.java
@@ -1,5 +1,17 @@
 package com.multiclientweb.repository;
 
+import com.multiclientweb.entity.ProductMgmt;
+
 public interface ProductMgmtRepository {
+
+	static ProductMgmt findByProductName(String productName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	static void save(ProductMgmt productmgmt) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
